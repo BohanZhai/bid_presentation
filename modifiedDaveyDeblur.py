fb66ef71f94632f2a2a801fd229f388f230afaf5
Hayden Brown <hayden@thebrowns.net.nz>
2018-06-03 02:35:33 +1200

    Algorithm improvements. Added snapshotting feature.

--- modifiedDaveyDeblur.py
+++ modifiedDaveyDeblur.py
@@ -1,11 +1,24 @@
 import numpy as np
 import math as m
 
-import writeMatrix as wm # Debugging
-import pdb # Debugging
+import os, shutil
+from PIL import Image, ImageFont, ImageDraw
 
-# Perform blind image deconvolution using the modified Davey algorithm
-def bid_davey(original_img, blurred_img, (psfWidth, psfHeight), iterations):
+import writeMatrix as wm # Debugging
+import pdb               # Debugging
+
+
+SNAPSHOT_DIR = './snapshots'
+
+# Perform blind image deconvolution using the modified Davey et al algorithm
+def bid_davey(original_img, blurred_img, (psfWidth, psfHeight), iterations, save_snapshots=False):
+
+    snapshot_filenames = []
+    if(save_snapshots) :
+        if os.path.exists(SNAPSHOT_DIR):
+            shutil.rmtree(SNAPSHOT_DIR)
+        os.mkdir(SNAPSHOT_DIR)
+
 
     result = 0                                    # Optimal restored image
 
@@ -51,47 +64,57 @@ def bid_davey(original_img, blurred_img, (psfWidth, psfHeight), iterations):
     print "Performing optimal deblur. Progress: [",
 
     # Used for progress bar display
-    countMAX = np.round(iterations / 10)
+    countMAX = np.round(iterations * 0.05) # 5% increments
     counter = 0
 
     G = np.fft.fft2(g, axes=(0,1))
 
-    for ii in range (0, iterations):
+    for iteration in range (0, iterations):
         counter += 1
         if (counter == countMAX):
             print ".",            # Progress bar display
             counter = 0
+            if (save_snapshots):
+                generateSnapshot(f, channels, iteration, SNAPSHOT_DIR, snapshot_filenames)
 
         F = np.fft.fft2(f, axes=(0,1))
         H = wiener(G, F)
-        h = np.fft.fftshift(np.fft.ifft2(H, axes=(0,1)))
+        h = np.fft.fftshift(np.fft.ifft2(H, axes=(0,1)), axes=(0,1))
         h = psfConstraint(h, (psfWidth, psfHeight), (supportWidth, supportHeight))
         H = np.fft.fft2(h, axes=(0,1))
         F = wiener(G, H)
-        f = np.fft.fftshift(np.fft.ifft2(F, axes=(0,1)))
+        f = np.fft.fftshift(np.fft.ifft2(F, axes=(0,1)), axes=(0,1))
         f = imageConstraint(f, (supportWidth, supportHeight))
 
-        if (ii == iterations - 1):
+        if (iteration == iterations - 1):
             print "] (complete)"
 
         # Calculate and store Mean Squared Error for iteration
         mse = computeMSE(f, original_img)
-        image_mse[ii, 0] = ii
-        image_mse[ii, 1] = mse
+        image_mse[iteration, 0] = iteration
+        image_mse[iteration, 1] = mse
 
-        if (ii == 0):
+        if (iteration == 0):
             min_mse = mse
             min_mse_iterations = 0
             result = f
 
         if (mse < min_mse):
             min_mse = mse
-            min_mse_iterations = ii
+            min_mse_iterations = iteration
             result = f
 
     result = result[origin_y:origin_y+supportHeight, origin_x:origin_x+supportWidth, 0:channels]
-    result = np.multiply(np.divide(result, np.max(result)), 255)
+
+    for ch in range (0, channels):
+        result[:, :, ch] = np.multiply(np.divide(result[:, :, ch], np.max(result[:, :, ch])), 255)
     result = np.abs(result).astype(np.uint8)
+
+    if(save_snapshots):
+        images = []
+        for filename in snapshot_filenames:
+            images.append(imageio.imread(filename))
+            imageio.mimsave(SNAPSHOT_DIR + '/snapshots.gif', images, format='GIF', duration=1)
 
     return (result, image_mse, min_mse, min_mse_iterations)
 
@@ -117,9 +140,6 @@ def psfConstraint(data, psf_dims, support):
 
     # TODO check whether whole thing should be set to zero if <0 -- or just individual real/complex parts
     # TODO need to re-add energy? from non-negative pixels?
-
-    # old
-    #data[origin_y:origin_y+supportHeight-1, origin_x:origin_x+supportWidth-1] = data[origin_y:origin_y+supportHeight-1, origin_x:origin_x+supportWidth-1].clip(min=0)
 
     return data
 
@@ -149,18 +169,8 @@ def imageConstraint(data, support):
 
 def wiener (num, den):
     beta = 0.5
-
-    W_1 = np.conj(den)
-    W_2 =  np.square(np.abs(den))
-    W_3 =  np.abs(den)
-    W_4 = np.square(np.abs(den))
-    W_5 = np.divide(beta, np.square(np.abs(den)) )
-    W_6 = np.square(np.abs(den)) + np.divide(beta, np.square(np.abs(den)) )
-
     W = np.divide(np.conj(den), np.square(np.abs(den)) + np.divide(beta, np.square(np.abs(den)) ) )
-
-    result = np.multiply(W, num)
-    return result
+    return np.multiply(W, num)
 
 
 # Calculate the Mean Squared Error (MSE) between two images
@@ -169,12 +179,23 @@ def computeMSE(img_estimate, src_image):
 
     # Normalise image estimate
     img_estimate = np.multiply(np.divide(img_estimate, np.max(img_estimate)), 255)
-
-    # print "img_estimate.shape = " + str(img_estimate.shape) + ", src_image.shape = " + str(src_image.shape)
-
     if (img_estimate.shape != src_image.shape):
         raise Exception('ERROR: images are not the same dimensions [computeMSE()].')
     else:
         mse = np.abs(np.square(np.subtract(img_estimate, src_image)).mean())
 
     return mse
+
+def generateSnapshot(f, channels, iteration, SNAPSHOT_DIR, snapshot_filenames):
+    for ch in range (0, channels):
+        snapshot = f
+        snapshot[:, :, ch] = np.multiply(np.divide(snapshot[:, :, ch], np.max(snapshot[:, :, ch])), 255)
+    snapshot = np.abs(snapshot).astype(np.uint8)
+    filename = SNAPSHOT_DIR + '/snap_' + '{:06d}'.format(iteration) + '.bmp'
+
+    img = Image.fromarray(snapshot[:,:,0],mode='L')
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.load_default()
+    draw.text((10, snapshot.shape[0]-10), "Snap-" + str(iteration), font=font, fill=255)
+    snapshot_filenames.append(filename)
+    img.save(filename)
